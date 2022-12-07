b3997d01875e171ee400853e651ecb9e5697f948
Petar Marinov <petar.marinov@accedia.com>
2022-12-07 17:57:24 +0200

    ignore styles and hover added

--- src/components/UnderlinedButton.tsx
+++ src/components/UnderlinedButton.tsx
@@ -2,15 +2,18 @@ import Button from '@mui/material/Button'
 import { styled } from '@mui/material/styles'
 
 export const UnderlinedButtons = styled(Button, {
-  shouldForwardProp: (props) => props !== 'outlined'
-})(({ variant }) => ({
-  variant,
+  shouldForwardProp: (prop) => prop !== 'variant'
+})(() => ({
+  border: '2px solid',
   borderColor: 'transparent',
   borderBottomColor: 'blue',
   borderRadius: 1,
   borderBottomWidth: 3,
   fontSize: 15,
-  fontWeight: 'bold'
+  fontWeight: 'bold',
+  '&:hover': {
+    backgroundColor: 'red'
+  }
 }))
 
 /* export const UnderlinedButton = (props: ButtonProps) => <Button variant='contained' {...props}/>
