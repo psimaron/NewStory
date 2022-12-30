4a0b42dcb8784a10402601b259ba85d1a23d2a57
Petar Marinov <petar.marinov@accedia.com>
2022-12-30 17:56:42 +0200

    working custom hook

--- src/components/SuperHeroAutocomplete.tsx
+++ src/components/SuperHeroAutocomplete.tsx
@@ -1,58 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-import axios from 'axios';
-
-const request = async (word: string) => {
-  const promise = new Promise((resolve) => {
-    setTimeout(
-      () =>
-        resolve(
-          axios.get(
-            `https://superhero-search.p.rapidapi.com/api/heroes/${word}`,
-            {
-              headers: {
-                'X-RapidAPI-Key':
-                  '0bc116f760mshc0b7d82293c0c2ep1e3397jsn632a8f5d22b2',
-                'X-RapidAPI-Host': 'superhero-search.p.rapidapi.com',
-              },
-            },
-          ),
-        ),
-      5000,
-    );
-  });
-  const results = await promise.catch((err) => console.log(err));
-  return results;
-};
-
-function useSuperHeroSearch(term: any) {
-  const [word, setWord] = useState('');
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [result, setResult] = useState('');
-
-  useEffect(() => {
-    setLoading(true);
-    setWord(term);
-    request(word)
-      .then((res: any) => setResult(res))
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => setLoading(false));
-    return clearTimeout(1);
-  }, [term]);
-  const final = [result, loading, error];
-  return final;
-}
+import { CircularProgress } from '@mui/material';
+import useSuperHeroSearch from './useSuperHeroSearch';
 
 export default function SuperHeroAutocomplete() {
-  const handleChange = (e: any) => {
-    const term = e.target.value;
-    console.log(term);
-  };
-  useSuperHeroSearch(handleChange);
+  const [term, setTerm] = useState('');
+  const { superHero, loading, error } = useSuperHeroSearch(term);
+  const handleChange = (e: any) => setTerm(e.target.value);
+  console.log(superHero, loading, error);
   return (
     <Autocomplete
       value=""
@@ -60,10 +16,12 @@ export default function SuperHeroAutocomplete() {
       clearOnBlur
       handleHomeEndKeys
       id="avengers"
-      options={['']}
-      renderOption={(option) => <li {...option}>hi</li>}
+      options={superHero}
+      renderOption={(option) => <li {...option}>Hi</li>}
       renderInput={(option) => (
-        <TextField onChange={handleChange} {...option} label="Avengers" />
+        <TextField onChange={handleChange} {...option} label="Heroes">
+          <CircularProgress />
+        </TextField>
       )}
     />
   );
