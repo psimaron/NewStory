2e56777ad1ed9db494036df094a7e6d6bf93250e
Petar Marinov <petar.marinov@accedia.com>
2023-01-06 14:47:49 +0200

    added a debounce method properly

--- src/components/SearchCarAutocomplete.tsx
+++ src/components/SearchCarAutocomplete.tsx
@@ -9,27 +9,42 @@ import useCarSearch from './useCarSearch';
 
 export interface TextCustomise {
   label?: string;
+  helperText?: string;
+  id?: string;
 }
+const debounce = (fn: Function, ms = 300) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (this: any, ...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn.apply(this, args), ms);
+  };
+};
 
 export default function SearchCarAutocomplete(props: TextCustomise) {
-  const { cars, loading, error, search } = useCarSearch();
-  const { label = 'Cars' } = props;
-
-  let throttlePause: boolean;
-  const throttle = (event: any, value: string) => {
-    if (throttlePause) return;
-    throttlePause = true;
-    setTimeout(() => {
-      if (value.length >= 3) {
-        search(value);
-      }
-      throttlePause = false;
-    }, 300);
+  const header = {
+    'X-RapidAPI-Key': '59f0db3649msh4cb00124f5c8564p191a6cjsnc468d27648d7',
+    'X-RapidAPI-Host': 'car-data.p.rapidapi.com',
   };
+  const { cars, loading, error, search } = useCarSearch(header);
+  const {
+    label = 'Cars',
+    helperText = 'There is an issue with your request',
+    id = '1',
+  } = props;
+
+  const handleChange = (value: string) => {
+    if (value.length >= 3) {
+      search(value);
+    }
+  };
+
+  const debouncedFunction = debounce(handleChange, 1000);
 
   return (
     <Autocomplete
-      onInputChange={(event, value) => throttle(event, value)}
+      onInputChange={(event, value) => {
+        debouncedFunction(value);
+      }}
       onChange={(event, value) => {
         const selectedOption = value;
         return selectedOption;
@@ -37,13 +52,14 @@ export default function SearchCarAutocomplete(props: TextCustomise) {
       selectOnFocus
       clearOnBlur
       handleHomeEndKeys
+      id={id}
       options={cars}
       getOptionLabel={(option) =>
         `${option.make} ${option.model} ${option.year}`
       }
       renderInput={(option) => (
         <TextField
-          helperText={error && 'There is an issue with your request'}
+          helperText={error && helperText}
           error={error}
           label={label}
           {...option}
--- src/components/useCarSearch.tsx
+++ src/components/useCarSearch.tsx
@@ -6,26 +6,17 @@ interface CarInfo {
   make: string;
 }
 
-function useCarSearch(keys?: any) {
+function useCarSearch(header: HeadersInit) {
   const [cars, setCars] = useState<CarInfo[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const defaultKeys = {
-    'X-RapidAPI-Key': '59f0db3649msh4cb00124f5c8564p191a6cjsnc468d27648d7',
-    'X-RapidAPI-Host': 'car-data.p.rapidapi.com',
-  };
-  let finalKeys: any;
-  if (keys == null) {
-    finalKeys = defaultKeys;
-  } else {
-    finalKeys = keys;
-  }
+
   const search = (word: string) => {
     setLoading(true);
 
-    const options = {
+    const options: RequestInit = {
       method: 'GET',
-      headers: finalKeys,
+      headers: header,
     };
 
     fetch(
@@ -34,13 +25,12 @@ function useCarSearch(keys?: any) {
     )
       .then((response: any) => response.json())
       .then((json) => setCars(json))
-      .catch((error: any) => {
+      .catch((error: Error) => {
         setError(true);
         console.log(error);
       })
       .finally(() => setLoading(false));
   };
-
   return { cars, loading, error, search };
 }
 
